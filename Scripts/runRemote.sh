1af2bdedfd8417e52f46e32e7c3128d121916ad1
tbaumeist <tbaumeist@gmail.com>
2011-09-29 05:24:11 -1000

    Updated IP to exclude the VPN IP

--- Scripts/runRemote.sh
+++ Scripts/runRemote.sh
@@ -79,7 +79,7 @@ then
 fi
 
 #Get local IP
-IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v '192.168.33' | cut -d: -f2 | awk '{ print $1}'`
 
 
 # start remote debug server
