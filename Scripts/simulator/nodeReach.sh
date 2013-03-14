f009a2651202e6e6caa72478ee70f6f0443d8337
U-WheelOfTime\Todd <Todd@WheelOfTime.(none)>
2013-03-14 16:07:31 -0700

    node reach script

--- Scripts/simulator/nodeReach.sh
+++ Scripts/simulator/nodeReach.sh
@@ -5,6 +5,7 @@ _userDir=$(pwd)
 _firstRun=0
 _firstRunInner=0
 _output="$_userDir/counterMeasure.dat.txt"
+_output2="$_userDir/analysis.dat.txt"
 _log="$_userDir/counterMeasure.log"
 
 source $_scriptDir/../common/general.sh
@@ -21,58 +22,15 @@ function run
   local command="$1 --graph-save-dot $3"
 
   local count=0
-  local innerHeader=""
-  local innerResult=""
   while read line
   do
     let count=count+1
-	
-	#echo "java -cp $_scriptDir/../../bin/freenet-route-prediction.jar  frp.gephi.rti.AttackNodeAnalysisSingle -t $3 -n $networkSize -p $degree -htl 6 -ds 0 -o graph.analysis.dat -maxhtl 6"
-	#echo "sim out"
-	#echo "$line"
-	#echo "sim out: end"
-	
-	if [ "$line" = "" ]
-	then
-		echo "Error generating graph"
-		continue
-	fi
-	
-    if [ $count -eq 1 ]
-    then 
-		local innerCount=0
-		echo "" > graph.analysis.dat	
-		echo "sleeping..."
-		sleep 20
-		
-		java -cp "$_scriptDir/../../bin/freenet-route-prediction.jar"  frp.gephi.rti.AttackNodeAnalysisSingle -t $3 -n $networkSize -p $degree -htl 4 -ds 0 -o graph.analysis.dat -maxhtl 4 2>>"$_log"
-		while read innerLine
-		do
- 			let innerCount=innerCount+1
-		
-			# only run this once
-			if [ $_firstRunInner -eq 0 ] 
-			then
-			  innerHeader=${innerLine// /-}
-			  innerHeader=${innerHeader//,/ }
-			  _firstRunInner=1
-			fi 
-			 #skip over firt line it is just the headers
-			if [ $innerCount -eq 1 ]
-			then 
-			  continue
-			fi
-			innerResult=${innerLine//,/ }
-			
-		done < <(cat graph.analysis.dat)
-    fi
-	
 	
     # only run this once
     if [ $_firstRun -eq 0 ] 
     then
       # setup the output data file column headers
-      echo "$_outputHeader $line $innerHeader" > "$_output"
+      echo "$_outputHeader $line" > "$_output"
       _firstRun=1
     fi 
 
@@ -83,10 +41,44 @@ function run
     fi
 	
     # save values to data file
-    echo "$2 $line $innerResult" >> "$_output"
+    echo "$2 $line" >> "$_output"
 
     
   done < <(java -Xms2500m -jar "$_scriptDir/../../bin/freenet-simulator.jar" $command 2>>"$_log")
+}
+
+#Parameters
+#1 sub-command
+#2 list of values
+function run2 
+{
+		echo "Analyzing... $2"
+		local innerCount=0
+		echo "" > graph.analysis.dat	
+		
+		echo "java -cp $_scriptDir/../../bin/freenet-route-prediction.jar  frp.gephi.rti.AttackNodeAnalysisSingle -t $3 -n $networkSize -p $degree -htl 4 -ds 0 -o graph.analysis.dat -maxhtl 4"
+		java -cp "$_scriptDir/../../bin/freenet-route-prediction.jar"  frp.gephi.rti.AttackNodeAnalysisSingle -t $3 -n $networkSize -p $degree -htl 4 -ds 0 -o graph.analysis.dat -maxhtl 4 2>>"$_log"
+		while read innerLine
+		do
+ 			let innerCount=innerCount+1
+		
+			# only run this once
+			if [ $_firstRunInner -eq 0 ] 
+			then
+			  innerHeader=${innerLine// /-}
+			  innerHeader=${innerHeader//,/ }
+			  echo "$_outputHeader $innerHeader" > "$_output2"
+			  _firstRunInner=1
+			fi 
+			 #skip over firt line it is just the headers
+			if [ $innerCount -eq 1 ]
+			then 
+			  continue
+			fi
+			innerResult=${innerLine//,/ }
+			echo "$2 $innerResult" >> "$_output2"
+			
+		done < <(cat graph.analysis.dat)
 }
 
 #Main entry point
@@ -130,7 +122,7 @@ do
           do
             for randomRoutingChance in $randomRoutingChances
             do
-	      command="--graph-size $networkSize $degreeDist $degree "
+	          command="--graph-size $networkSize $degreeDist $degree "
               command+="$topType --route-look-ahead $lookAhead  "
               command+="--route-look-precision $lookPrecisLoss "
               command+="--route-random-chance $randomRoutingChance "
@@ -140,6 +132,38 @@ do
               values+="$topType $lookAhead $lookPrecisLoss $randomRoutingChance"
 
               run "$command" "$values" "$networkSize.$degree.$topType.dot"
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
+for lookAhead in $lookAheads
+do
+  for degree in $degrees
+  do
+    for networkSize in $networkSizes
+    do
+      for degreeDist in $degreeDists
+      do
+        for topType in $topTypes
+        do
+          for lookPrecisLoss in $lookPrecisLosses
+          do
+            for randomRoutingChance in $randomRoutingChances
+            do
+			  command="--graph-size $networkSize $degreeDist $degree "
+              command+="$topType --route-look-ahead $lookAhead  "
+              command+="--route-look-precision $lookPrecisLoss "
+              command+="--route-random-chance $randomRoutingChance "
+              command+="$constCommand"
+
+              values="$_experimentNum $networkSize $degree $degreeDist "
+              values+="$topType $lookAhead $lookPrecisLoss $randomRoutingChance"
+
+              run2 "$command" "$values" "$networkSize.$degree.$topType.dot"
             done
           done
         done
